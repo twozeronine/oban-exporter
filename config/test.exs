0ef8c8cb29f5f0919c2bd8e26b936a4dfbd4821d
twozeronine <amdusias209@gmail.com>
2023-11-29 16:33:00 +0900

    ⚙️ Fix hostname

--- config/test.exs
+++ config/test.exs
@@ -8,7 +8,7 @@ import Config
 config :oban_exporter, ObanExporter.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "127.0.0.1",
   database: "oban_exporter_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10,
