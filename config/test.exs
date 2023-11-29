138b75e094f66412d5333e6a8f56259768bac0c6
twozeronine <amdusias209@gmail.com>
2023-11-29 16:09:33 +0900

    ⚙️ Set port

--- config/test.exs
+++ config/test.exs
@@ -11,7 +11,8 @@ config :oban_exporter, ObanExporter.Repo,
   hostname: "localhost",
   database: "oban_exporter_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: 10
+  pool_size: 10,
+  port: 5432
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
