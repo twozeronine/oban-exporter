22e66a7ca03eadb035ada9ea4f21c5908686edc4
twozeronine <amdusias209@gmail.com>
2023-11-29 16:00:37 +0900

    🔨 Fix CI

--- mix.exs
+++ mix.exs
@@ -58,7 +58,6 @@ defmodule ObanExporter.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: [
-        "ecto.drop",
         "ecto.create --quiet",
         "ecto.migrate",
         "run priv/repo/seeds.exs",
